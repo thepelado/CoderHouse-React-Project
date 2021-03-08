85582871b2d868600a8a96d9ecfca7ffed298c8e
Cristian Brunacci <brunacci.cristian@gmail.com>
2021-03-08 14:55:58 -0300

    Entrega Final

--- src/data/productos.js
+++ src/data/productos.js
@@ -1,5 +1,60 @@
 export const productos = [
-    /*{
+    {
+        title: "Logitech B100 negro – Mouse",
+        brand: "Logitech",
+        categoryID: 1,
+        category: "Mouse",
+        stock: 10,
+        price: "600",
+        photo: "https://media.lifeinformatica.com/contents/Life/LOGITECH-910-003357/imgs/910-003357-01.jpg",
+        description: "Un ratón básico de oficina. Dispone de un control con seguimiento óptico suave y sensible y con una construcción para ambidiestros. Gracias a su conectividad Plug and Play estará listo para usar al momento.",
+        sku: "RALOG004"
+    },
+    {
+        title: "Cooler Master MK850 RGB MX red Aimpad – Teclado",
+        brand: "Cooler Master",
+        categoryID: 2,
+        category: "Teclados",
+        stock: 10,
+        price: "9599",
+        photo: "https://media.lifeinformatica.com/contents/Life/COOLER_MASTER-MK-850-GKCR1-SP/imgs/MK-850-GKCR1-SP-01.jpg",
+        description: "El teclado mecánico MK850 está totalmente equipado con la exclusiva tecnología Aimpad para el control analógico. Transición perfecta de MOBA a FPS a juegos de conducción e incluso simuladores de vuelo con un control sin precedentes en un grupo de ocho teclas (QWERASDF). Presentado con los interruptores rojos Cherry MX para mayor durabilidad, ruedas de precisión para ajustes fáciles en el juego y aluminio cepillado para durar en todas tus batallas. El MK850, combinado con el cable extraíble tipo C, el reposamuñecas magnético y la retroiluminación RGB por tecla, es el siguiente paso en la tecnología de teclados para gaming.",
+        sku: "TECOL019"
+    },
+    {
+        title: "Logitech C922 Pro Stream – Webcam",
+        brand: "Logitech",
+        categoryID: 3,
+        category: "Webcams",
+        stock: 10,
+        price: "12500",
+        photo: "https://media.lifeinformatica.com/contents/Life/LOGITECH-960-001088/imgs/960-001088-01.jpg",
+        description: "Webcam Logitech HD Pro Webcam C922. Lente de alta velocidad de enfoque con rango de apertura de 78º. Corrección de iluminación HD para escenarios con poca luz o con exceso de luz. Incorpora un micrófono dual stereo. Permite streaming a resolución HD 1080p a 30fps y 720p a 60fps.",
+        sku: "WCLOG010"
+    },
+    {
+        title: "Monitor 27p Benq Zowie Xl2731 144hz Gamer Full Hd",
+        brand: "Benq",
+        categoryID: 4,
+        category: "Monitores",
+        stock: 10,
+        price: "84800",
+        photo: "https://media.lifeinformatica.com/contents/Life/BENQ-9H.LGMLB.QBE/imgs/9H.LGMLB.QBE-01.jpg",
+        description: "La serie XL cuenta con los nuevos monitores de PC para e-sports, afinados para garantizar la experiencia más suave y sensible, así como las imágenes más claras: tus armas para la competición.",
+        sku: "TFBEN082"
+    },
+    {
+        title: "Ducky One 2 Mini Pure White RGB MX Brown Layout ES – Teclado",
+        brand: "Ducky",
+        categoryID: 2,
+        category: "Teclados",
+        stock: 5,
+        price: "7990",
+        photo: "https://media.lifeinformatica.com/contents/Life/DUCKY-DKON2061ST-BESALWWT1/imgs/DKON2061ST-BESALWWT1-01.jpg",
+        description: "Ducky One 2 Mini Pure White RGB con configuración de teclas en Español (SP). El teclado Ducky One 2 Mini Pure White RGB en color blanco puro es el teclado gaming miniaturizado para profesionales. Ahora con un nuevo bisel más elegante y efectos RGB totalmente personalizables. Rendimiento excepcional duradero y confiable. Incorpora interruptores (switch) de fabricante lider alemán Cherry MX Brown. Utiliza un USB tipo C desconectable con una frecuencia más alta de velocidad de sondeo de 1000Hz. Comfort gracias a su ajuste de altura de 3 niveles. Fabricado con un PCB de 4 capas de mayor rendimiento.",
+        sku: "TEDUC042"
+    },
+    {
         title: "Sharkoon Elbrus 2 Black / Blue – Silla",
         brand: "Sharkoon",
         categoryID: "89lESI5BYGXrqW0NmdXH",
@@ -38,7 +93,7 @@ export const productos = [
         photo: "https://media.lifeinformatica.com/contents/Life/WD-WDBU6Y0020BBK-EESN/imgs/WDBU6Y0020BBK-EESN-01.jpg",
         description: "El WD Elements Portable es una solución sencilla y de confianza para almacenar tus datos más preciados. Con un tamaño de tan solo 2.5, el pequeño WD Elements Portable te permite guardar hasta 2TB de fotos, vídeos, o los archivos que prefieras. Es un disco duro económico pero de una marca líder como es WD, y su diseño en color negro hace que tanto hombres como mujeres puedan combinarlo con su estilo. Tiene conexión USB 3.0 para ofrecer altas velocidades de transferencia de archivos.",
         sku: "HEWESA6"
-    },*/
+    },
     {
         title: "WD My Book 6TB USB 3.0 3.5″ – Disco Duro Externo",
         brand: "WD",
@@ -58,7 +113,7 @@ export const productos = [
         photo: "https://media.lifeinformatica.com/contents/Life/MICROSOFT-QAS-00002/imgs/QAS-00002-01.jpg",
         description: "Experimente el diseño modernizado del control inalámbrico Xbox en Robot White, con superficies esculpidas y geometría refinada para una mayor comodidad durante el juego. Manténgase en el objetivo con un D-pad híbrido y agarre texturizado en los gatillos, parachoques y carcasa trasera. Capture y comparta contenido sin problemas, como capturas de pantalla, grabaciones y más con el nuevo botón Compartir. Utilice la aplicación Accesorios de Xbox para reasignar botones y crear perfiles de control personalizados para sus juegos favoritos. Empareje, juegue y cambie rápidamente entre dispositivos, incluidos Xbox Series X, Xbox One, PC con Windows 10 y Android, la compatibilidad con iOS estará disponible en el futuro. Conecte cualquier auricular compatible con el conector de audio de 3,5 mm. *Microsoft Xbox Wireless Controller White. Tipo de dispositivo: Gamepad, Plataformas de juego soportadas: Xbox Series S,Xbox Series X,Xbox One,Xbox One S,Xbox One X, Tecnología de control para juegos: Analógico/Digital. Tecnología de conectividad: Inalámbrico y alámbrico, Interfaz del dispositivo: Bluetooth/USB, Interfaz de receptor inalámbrico: USB. Color del producto: Blanco. Fuente de energía: Batería. Sistema operativo Windows soportado: Windows 10, Sistemas operativos móviles soportados: Android",
         sku: "JOMIC019"
-    }/*,
+    },
     {
         title: "Logitech G920 Driving Force PC/XBOX – Volante y Pedales",
         brand: "Logitech",
@@ -68,5 +123,5 @@ export const productos = [
         photo: "https://media.lifeinformatica.com/contents/Life/LOGITECH-941-000123/imgs/941-000123-01.jpg",
         description: "La simulación de conducción inmersiva cobra vida en los detalles. El efecto Force Feedback con dos motores deja sentir claramente cada derrapada y particularidad del terreno. Los cojinetes de acero del eje del volante aportan peso y durabilidad. Las palancas de cambio y los pedales de acero inoxidable aplican fuerza con precisión. El volante con recubrimiento de cuero cosido a mano pone un toque de lujo en cada coche. Los engranajes helicoidales ofrecen una silenciosa fluidez.",
         sku: "JOLOG009"
-    }*/
+    }
 ];
